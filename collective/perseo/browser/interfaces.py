3528825d718a43e5a14b0a35fe41cd3a27d2c6a6
Federica D'Elia <federica.delia@redturtle.it>
2011-07-13 08:18:43 +0000

    Add Plone SEO control panel

---
+++ collective/perseo/browser/interfaces.py
@@ -0,0 +1,6 @@
+from plone.theme.interfaces import IDefaultPloneLayer
+
+class IPerSEOLayer(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 skin layer bound to a Skin
+       Selection in portal_skins.
+    """
---
+++ collective/perseo/browser/seo_config.py
@@ -0,0 +1,49 @@
+from zope.interface import Interface
+from zope.interface import implements
+from zope.component import adapts
+
+from plone.fieldsets.fieldsets import FormFieldsets
+from plone.app.controlpanel.form import ControlPanelForm
+
+from Products.CMFDefault.formlib.schema import SchemaAdapterBase
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+from collective.perseo import perseoMessageFactory as _
+
+class ISEOConfigBaseSchema(Interface):
+    """"""
+class ISEOConfigAdvancedSchema(Interface):
+    """"""
+
+class ISEOConfigSchema(ISEOConfigBaseSchema,
+                       ISEOConfigAdvancedSchema):
+    """Combined schema for the adapter lookup.
+    """
+
+class SEOConfigAdapter(SchemaAdapterBase):
+
+    adapts(IPloneSiteRoot)
+    implements(ISEOConfigSchema)
+
+    def __init__(self, context):
+        super(SEOConfigAdapter, self).__init__(context)
+
+# Fieldset configurations
+baseset = FormFieldsets(ISEOConfigBaseSchema)
+baseset.id = 'seobase'
+baseset.label = _(u'label_seobase', default=u'Base')
+
+advancedset = FormFieldsets(ISEOConfigAdvancedSchema)
+advancedset.id = 'seoadvanced'
+advancedset.label = _(u'label_seoadvanced', default=u'Advanced')
+
+class SEOConfig(ControlPanelForm):
+
+    form_fields = FormFieldsets(baseset, advancedset)
+
+    label = _("Plone SEO Configuration")
+    description = _("seo_configlet_description", default="You can select what "
+                    "content types are qSEOptimizer-enabled, and control if "
+                    "Dublin Core metatags are exposed in the header of content "
+                    "pages.")
+    form_name = _("")
