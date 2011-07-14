3fbccf79b6505a94345625bc3022f64c5817093c
Federica D'Elia <federica.delia@redturtle.it>
2011-07-14 08:06:05 +0000

    Added various fields to Title tab + Added widgets to fields in Plone SEO control panel tool

--- collective/perseo/browser/seo_config.py
+++ collective/perseo/browser/seo_config.py
@@ -1,7 +1,9 @@
 from zope.interface import Interface
 from zope.interface import implements
 from zope.component import adapts
-from zope.schema import TextLine
+from zope.schema import TextLine, Text
+
+from zope.app.form.browser import TextAreaWidget, TextWidget
 
 from plone.fieldsets.fieldsets import FormFieldsets
 from plone.app.controlpanel.form import ControlPanelForm
@@ -35,6 +37,36 @@ class ISEOConfigWMToolsSchema(Interface):
 class ISEOConfigTitleSchema(Interface):
     """Schema for Title"""
     
+    homepage_title = TextLine(
+        title=_("label_homepage_title",
+                default=u"Home Page Title"),
+        required=False)
+    
+    homepage_description = Text(
+        title=_("label_homepage_description",
+                default=u"Home Page Description"),
+        required=False)
+    
+    homepage_keywords = TextLine(
+        title=_("label_homepage_keywords",
+                default=u"Home Page Keywords"),
+        required=False)
+    
+    singlepage_title = TextLine(
+        title=_("label_singlepage_title",
+                default=u"Single Page Title"),
+        required=False)
+    
+    singlepage_description = Text(
+        title=_("label_singlepage_description",
+                default=u"Single Page Description"),
+        required=False)
+    
+    singlepage_keywords = TextLine(
+        title=_("label_singlepage_keywords",
+                default=u"Single Page Keywords"),
+        required=False)
+    
 
 class ISEOConfigSchema(ISEOConfigWMToolsSchema,
                        ISEOConfigTitleSchema):
@@ -52,6 +84,12 @@ class SEOConfigAdapter(SchemaAdapterBase):
     googleWebmasterTools = ProxyFieldProperty(ISEOConfigSchema['googleWebmasterTools'])
     yahooSiteExplorer = ProxyFieldProperty(ISEOConfigSchema['yahooSiteExplorer'])
     bingWebmasterTools = ProxyFieldProperty(ISEOConfigSchema['bingWebmasterTools'])
+    homepage_title = ProxyFieldProperty(ISEOConfigSchema['homepage_title'])
+    homepage_description = ProxyFieldProperty(ISEOConfigSchema['homepage_description'])
+    homepage_keywords = ProxyFieldProperty(ISEOConfigSchema['homepage_keywords'])
+    singlepage_title = ProxyFieldProperty(ISEOConfigSchema['singlepage_title'])
+    singlepage_description = ProxyFieldProperty(ISEOConfigSchema['singlepage_description'])
+    singlepage_keywords = ProxyFieldProperty(ISEOConfigSchema['singlepage_keywords'])
 
 # Fieldset configurations
 
@@ -66,6 +104,18 @@ titleset.label = _(u'label_seotitle', default=u'Title')
 class PerSEOConfig(ControlPanelForm):
 
     form_fields = FormFieldsets(wmtoolsset, titleset)
+    
+    form_fields['googleWebmasterTools'].custom_widget = TextWidget
+    form_fields['yahooSiteExplorer'].custom_widget = TextWidget
+    form_fields['bingWebmasterTools'].custom_widget = TextWidget
+    form_fields['homepage_title'].custom_widget = TextWidget
+    form_fields['homepage_description'].custom_widget = TextAreaWidget
+    form_fields['homepage_description'].custom_widget.height = 3
+    form_fields['homepage_keywords'].custom_widget = TextWidget
+    form_fields['singlepage_title'].custom_widget = TextWidget
+    form_fields['singlepage_description'].custom_widget = TextAreaWidget
+    form_fields['singlepage_description'].custom_widget.height = 3
+    form_fields['singlepage_keywords'].custom_widget = TextWidget
 
     label = _("Plone SEO Configuration")
     description = _("seo_configlet_description", default="You can select what "
