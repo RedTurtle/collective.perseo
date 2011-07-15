9d43919a46ab0465c18f252da80a3691b2ff3efc
Federica D'Elia <federica.delia@redturtle.it>
2011-07-15 09:22:13 +0000

    Added some views that allows to calculate title tag base on the type of context + Added some methods that allows to calculate title tag base on the page in PloneSiteRoot context

--- collective/perseo/browser/views.py
+++ collective/perseo/browser/views.py
@@ -23,12 +23,17 @@ class PerSEOContext(BrowserView):
         self.gseo = queryAdapter(self.pps.portal(), ISEOConfigSchema)
         self._perseo_metatags = self._getPerSEOMetaTags()
 
+    def __getitem__(self, key):
+        return self._perseo_metatags.get(key, '')
+    
     @view.memoize
     def _getPerSEOMetaTags(self):
         perseo_metatags = {
             "googleWebmasterTools": self.seo_globalGoogleWebmasterTools(),
             "yahooSiteExplorer": self.seo_globalYahooSiteExplorer(),
             "bingWebmasterTools":self.seo_globalBingWebmasterTools(),
+            "has_perseo_title":True,
+            "perseo_title":self.perseo_title(),
             }
         return perseo_metatags
     
@@ -58,3 +63,63 @@ class PerSEOContext(BrowserView):
         if self.gseo:
             result = self.gseo.bingWebmasterTools
         return result
+    
+    def get_gseo_field( self, field ):
+        """ Returned field from Plone SEO Configuration Control Panel Tool
+        """
+        result = ''
+        if self.gseo:
+            result = getattr(self.gseo, field, u'')
+        return result
+    
+    def perseo_title( self ):
+        return None
+
+class PerSEOContextPloneSiteRoot(PerSEOContext):
+    """ Calculate html header meta tags on context. Context == PloneSiteRoot
+    """
+    
+    def perseo_what_page(self):
+        # I take template_id as is done in ploneview
+        # if all goes well for ploneview is fine in my general view
+        template_id = None
+        if 'PUBLISHED' in self.request:
+            if getattr(self.request['PUBLISHED'], 'getId', None):
+                # template inside skins   
+                template_id = self.request['PUBLISHED'].getId()
+            if getattr(self.request['PUBLISHED'], __name__, None):
+                # template inside browser view
+                template_id = self.request['PUBLISHED'].__name__
+        
+        if template_id:
+            if template_id == 'search' or template_id == 'search_form':
+                return 'searchpage'
+            elif template_id == 'author':
+                return 'authorpage'
+            elif template_id == 'sitemap':
+                return 'sitemappage'
+            elif template_id == 'accessibility-info':
+                return 'accessibilitypage'
+            elif template_id == 'contact-info':
+                return 'contactpage'
+            else:
+                return 'homepage'
+        else:
+            return 'homepage'
+
+    def perseo_title( self ):
+        page = self.perseo_what_page()
+        return self.get_gseo_field('%s_title' % page)
+            
+class PerSEOContextATDocument(PerSEOContext):
+    """ Calculate html header meta tags on context. Context == ATDocument
+    """
+    def perseo_title( self ):
+        context = self.pcs.context
+        parent = self.pcs.parent()
+        
+        if parent == self.pps.portal() and parent.getDefaultPage() == context.id:
+            # this document is the home page
+            return self.get_gseo_field('homepage_title')
+        else:
+            return self.get_gseo_field('singlepage_title')
