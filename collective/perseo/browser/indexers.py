fa25d4c68f31917b9028f8f05f7a12f6a011e1c8
Federica D'Elia <federica.delia@redturtle.it>
2011-07-27 13:47:54 +0000

    fix

--- collective/perseo/browser/indexers.py
+++ collective/perseo/browser/indexers.py
@@ -2,27 +2,11 @@
 from Products.Archetypes.interfaces.base import IBaseContent
 from plone.indexer.decorator import indexer
 
-from zope.app.component.hooks import getSite
-from zope.component import queryAdapter
-
-from collective.perseo.browser.seo_config import ISEOConfigSchema
-
-def perseo_included_types(object):
-    portal = getSite()
-    gseo = queryAdapter(portal, ISEOConfigSchema)
-    if gseo:
-        return getattr(gseo, 'not_included_types', ())
-    return ()
-
 @indexer(IBaseContent)
 def get_included_in_sitemapxml(object, **kw):
-    included_types = perseo_included_types(object)
-    
-    default = object.portal_type in included_types
-    
     if object.hasProperty('pSEO_included_in_sitemapxml'):
-        return object.getProperty('pSEO_included_in_sitemapxml', default)
-    return default
+        return object.getProperty('pSEO_included_in_sitemapxml', None)
+    return None
 
 @indexer(IBaseContent)
 def get_priority_sitemapxml(object, **kw):
--- collective/perseo/browser/sitemap.py
+++ collective/perseo/browser/sitemap.py
@@ -1,6 +1,10 @@
 from plone.app.layout.sitemap.sitemap import SiteMapView
 from Products.CMFCore.utils import getToolByName
 
+from zope.component import queryAdapter
+
+from collective.perseo.browser.seo_config import ISEOConfigSchema
+
 class PerSEOSiteMapView (SiteMapView):
     """Creates the sitemap as explained in the specifications.
 
@@ -8,6 +12,17 @@ class PerSEOSiteMapView (SiteMapView):
     """
     def __init__(self, context, request):
         super(PerSEOSiteMapView, self).__init__(context, request)
+        self.gseo = queryAdapter(self.context, ISEOConfigSchema)
+        
+    def get_gseo_field( self, field, default=None):
+        """ Returned field from Plone SEO Configuration Control Panel Tool
+        """
+        if self.gseo:
+            return getattr(self.gseo, field, default)
+        return default
+        
+    def perseo_included_types(self):
+        return self.get_gseo_field('not_included_types',default=())
         
     def objects(self):
         """Returns the data to create the sitemap."""
@@ -15,7 +30,14 @@ class PerSEOSiteMapView (SiteMapView):
         catalog = getToolByName(self.context, 'portal_catalog')
         
         for item in catalog.searchResults({'Language': 'all'}):
-            if item.getIncludedInSitemapxml:
+            
+            included_types = self.perseo_included_types()
+            if item.getIncludedInSitemapxml != None:
+                include = item.getIncludedInSitemapxml
+            else:
+                include = item.portal_type in included_types
+            
+            if include:
                 if item.getPrioritySitemapxml:
                     yield {
                         'loc': item.getURL(),
