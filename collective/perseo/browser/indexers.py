9a1e2aecc6463fe4223dbfb653b749a403d1daf1
Federica D'Elia <federica.delia@redturtle.it>
2011-07-27 08:22:20 +0000

    fix

--- collective/perseo/browser/indexers.py
+++ collective/perseo/browser/indexers.py
@@ -1,7 +1,8 @@
-from zope.interface import Interface
+#from zope.interface import Interface
+from Products.Archetypes.interfaces.base import IBaseContent
 from plone.indexer.decorator import indexer
 
-@indexer(Interface)
+@indexer(IBaseContent)
 def get_included_in_sitemapxml(object, **kw):
     if object.hasProperty('pSEO_included_in_sitemapxml'):
         return object.getProperty('pSEO_included_in_sitemapxml', True)
