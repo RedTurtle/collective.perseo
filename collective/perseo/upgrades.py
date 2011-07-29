8e885946a1f493c36ddff0fb635cc7aa265c6889
Federica D'Elia <federica.delia@redturtle.it>
2011-07-29 14:13:56 +0000

    fix

--- collective/perseo/upgrades.py
+++ collective/perseo/upgrades.py
@@ -4,21 +4,22 @@ from zope.annotation.interfaces import IAnnotations
 from zope.component import queryMultiAdapter
 
 logger = logging.getLogger('collective.perseo')
-REMOVE_SEOPROPERTIES = ['pSEO_title'
-                        'pSEO_description'
-                        'pSEO_keywords'
-                        'pSEO_robots_advanced'
-                        'pSEO_robots_follow'
-                        'pSEO_robots_index'
-                        'pSEO_canonical'
-                        'pSEO_included_in_sitemapxml'
-                        'pSEO_priority_sitemapxml'
+REMOVE_SEOPROPERTIES = ['pSEO_title',
+                        'pSEO_description',
+                        'pSEO_keywords',
+                        'pSEO_robots_advanced',
+                        'pSEO_robots_follow',
+                        'pSEO_robots_index',
+                        'pSEO_canonical',
+                        'pSEO_included_in_sitemapxml',
+                        'pSEO_priority_sitemapxml',
                         'pSEO_itemtype']
 
 def toAnnotation(obj):
     for property in REMOVE_SEOPROPERTIES:
         if obj.hasProperty(property):
             value = obj.getProperty(property,None)
+            if type(value) == type(()): value = list(value)
             obj.manage_delProperties([property])
             annotations = IAnnotations(obj)
             annotations[property] = value
