a1722b039d35f615fd1e57d84eee0e3aead094fd
Federica D'Elia <federica.delia@redturtle.it>
2011-12-13 14:35:37 +0000

    Added accordion and proper style to nested fieldsets in control-panel template

--- collective/perseo/browser/seo_config.py
+++ collective/perseo/browser/seo_config.py
@@ -14,6 +14,7 @@ from plone.app.controlpanel.widgets import MultiCheckBoxThreeColumnWidget
 from Products.CMFDefault.formlib.schema import SchemaAdapterBase, ProxyFieldProperty
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.CMFPlone.utils import getToolByName, safe_unicode
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from collective.perseo import perseoMessageFactory as _
 from collective.perseo.browser.types import BAD_TYPES
@@ -48,9 +49,9 @@ class ISEOConfigWMToolsSchema(Interface):
         title=_("label_tracking_code_footer",
                 default=u"Tracking Code Footer"),
         required=False)
-    
-class ISEOConfigTitleSchema(Interface):
-    """Schema for Title"""
+
+class ISEOConfigTitleSchema_homepage(Interface):
+    """Schema for Title homepage"""
     
     homepage_title = TextLine(
         title=_("label_homepage_title",
@@ -66,7 +67,10 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_homepage_keywords",
                 default=u"Home Page Keywords"),
         required=False)
-    
+
+class ISEOConfigTitleSchema_singlepage(Interface):
+    """Schema for Title singlepage"""
+  
     singlepage_title = TextLine(
         title=_("label_singlepage_title",
                 default=u"Single Page Title"),
@@ -81,7 +85,10 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_singlepage_keywords",
                 default=u"Single Page Keywords"),
         required=False)
-    
+
+class ISEOConfigTitleSchema_searchpage(Interface):
+    """Schema for Title searchpage"""
+        
     searchpage_title = TextLine(
         title=_("label_searchpage_title",
                 default=u"Search Page Title"),
@@ -96,7 +103,10 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_searchpage_keywords",
                 default=u"Search Page Keywords"),
         required=False)
-    
+
+class ISEOConfigTitleSchema_notfoundpage(Interface):
+    """Schema for Title notfoundpage"""
+        
     notfoundpage_title = TextLine(
         title=_("label_notfoundpage_title",
                 default=u"Not Found Page Title"),
@@ -111,7 +121,10 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_notfoundpage_keywords",
                 default=u"Not Found Page Keywords"),
         required=False)
-    
+
+class ISEOConfigTitleSchema_authorpage(Interface):
+    """Schema for Title authorpage"""
+        
     authorpage_title = TextLine(
         title=_("label_authorpage_title",
                 default=u"Author Page Title"),
@@ -126,7 +139,10 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_authorpage_keywords",
                 default=u"Author Page Keywords"),
         required=False)
-    
+
+class ISEOConfigTitleSchema_sitemappage(Interface):
+    """Schema for Title sitemappage"""
+        
     sitemappage_title = TextLine(
         title=_("label_sitemappage_title",
                 default=u"Site map Title"),
@@ -141,6 +157,9 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_sitemappage_keywords",
                 default=u"Site map Keywords"),
         required=False)
+
+class ISEOConfigTitleSchema_accessibilitypage(Interface):
+    """Schema for Title accessibilitypages"""
     
     accessibilitypage_title = TextLine(
         title=_("label_accessibilitypage_title",
@@ -156,6 +175,9 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_accessibilitypage_keywords",
                 default=u"Accessibility Keywords"),
         required=False)
+
+class ISEOConfigTitleSchema_contactpage(Interface):
+    """Schema for Title contactpage"""
     
     contactpage_title = TextLine(
         title=_("label_contactpage_title",
@@ -171,6 +193,9 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_contactpage_keywords",
                 default=u"Contact Keywords"),
         required=False)
+
+class ISEOConfigTitleSchema_event(Interface):
+    """Schema for Title event"""
     
     event_title = TextLine(
         title=_("label_event_title",
@@ -186,6 +211,9 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_event_keywords",
                 default=u"Event Keywords"),
         required=False)
+
+class ISEOConfigTitleSchema_file(Interface):
+    """Schema for Title file"""
     
     file_title = TextLine(
         title=_("label_file_title",
@@ -201,7 +229,10 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_file_keywords",
                 default=u"File Keywords"),
         required=False)
-    
+
+class ISEOConfigTitleSchema_folder(Interface):
+    """Schema for Title folder"""
+   
     folder_title = TextLine(
         title=_("label_folder_title",
                 default=u"Folder Title"),
@@ -216,7 +247,10 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_folder_keywords",
                 default=u"Folder Keywords"),
         required=False)
-    
+
+class ISEOConfigTitleSchema_image(Interface):
+    """Schema for Title image"""
+        
     image_title = TextLine(
         title=_("label_image_title",
                 default=u"Image Title"),
@@ -231,7 +265,10 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_image_keywords",
                 default=u"Image Keywords"),
         required=False)
-    
+
+class ISEOConfigTitleSchema_link(Interface):
+    """Schema for Title link"""
+       
     link_title = TextLine(
         title=_("label_link_title",
                 default=u"Link Title"),
@@ -246,7 +283,10 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_link_keywords",
                 default=u"Link Keywords"),
         required=False)
-    
+
+class ISEOConfigTitleSchema_newsItem(Interface):
+    """Schema for Title newsItem"""
+       
     newsItem_title = TextLine(
         title=_("label_newsItem_title",
                 default=u"NewsItem Title"),
@@ -261,7 +301,10 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_newsItem_keywords",
                 default=u"NewsItem Keywords"),
         required=False)
-    
+
+class ISEOConfigTitleSchema_topic(Interface):
+    """Schema for Title topic"""
+  
     topic_title = TextLine(
         title=_("label_topic_title",
                 default=u"Topic Title"),
@@ -276,6 +319,23 @@ class ISEOConfigTitleSchema(Interface):
         title=_("label_topic_keywords",
                 default=u"Topic Keywords"),
         required=False)
+    
+class ISEOConfigTitleSchema(ISEOConfigTitleSchema_homepage,
+                            ISEOConfigTitleSchema_singlepage,
+                            ISEOConfigTitleSchema_searchpage,
+                            ISEOConfigTitleSchema_notfoundpage,
+                            ISEOConfigTitleSchema_authorpage,
+                            ISEOConfigTitleSchema_sitemappage,
+                            ISEOConfigTitleSchema_accessibilitypage,
+                            ISEOConfigTitleSchema_contactpage,
+                            ISEOConfigTitleSchema_event,
+                            ISEOConfigTitleSchema_file,
+                            ISEOConfigTitleSchema_folder,
+                            ISEOConfigTitleSchema_image,
+                            ISEOConfigTitleSchema_link,
+                            ISEOConfigTitleSchema_newsItem,
+                            ISEOConfigTitleSchema_topic):
+    """Schema for Title"""
     
 class ISEOConfigIndexingSchema(Interface):
     """Schema for Indexing"""
@@ -559,7 +619,71 @@ wmtoolsset = FormFieldsets(ISEOConfigWMToolsSchema)
 wmtoolsset.id = 'seowmtools'
 wmtoolsset.label = _(u'label_seowmtools', default=u'WM Tools')
 
-titleset = FormFieldsets(ISEOConfigTitleSchema)
+titleset_homepage = FormFieldsets(ISEOConfigTitleSchema_homepage)
+titleset_homepage.id = 'seotitle_homepage'
+titleset_homepage.label = _(u'label_seotitle_homepage', default=u'Home Page')
+
+titleset_singlepage = FormFieldsets(ISEOConfigTitleSchema_singlepage)
+titleset_singlepage.id = 'seotitle_singlepage'
+titleset_singlepage.label = _(u'label_seotitle_singlepage', default=u'Single Pages')
+
+titleset_searchpage = FormFieldsets(ISEOConfigTitleSchema_searchpage)
+titleset_searchpage.id = 'seotitle_searchpage'
+titleset_searchpage.label = _(u'label_seotitle_searchpage', default=u'Search Page')
+
+titleset_notfoundpage = FormFieldsets(ISEOConfigTitleSchema_notfoundpage)
+titleset_notfoundpage.id = 'seotitle_notfoundpage'
+titleset_notfoundpage.label = _(u'label_seotitle_notfoundpage', default=u'Not Found Page')
+
+titleset_authorpage = FormFieldsets(ISEOConfigTitleSchema_authorpage)
+titleset_authorpage.id = 'seotitle_authorpage'
+titleset_authorpage.label = _(u'label_seotitle_authorpage', default=u'Author Page')
+
+titleset_sitemappage = FormFieldsets(ISEOConfigTitleSchema_sitemappage)
+titleset_sitemappage.id = 'seotitle_sitemappage'
+titleset_sitemappage.label = _(u'label_seotitle_sitemappage', default=u'Site Map')
+
+titleset_accessibilitypage = FormFieldsets(ISEOConfigTitleSchema_accessibilitypage)
+titleset_accessibilitypage.id = 'seotitle_accessibilitypage'
+titleset_accessibilitypage.label = _(u'label_seotitle_accessibilitypage', default=u'Accessibility')
+
+titleset_contactpage = FormFieldsets(ISEOConfigTitleSchema_contactpage)
+titleset_contactpage.id = 'seotitle_contactpage'
+titleset_contactpage.label = _(u'label_seotitle_contactpage', default=u'Contact')
+
+titleset_event = FormFieldsets(ISEOConfigTitleSchema_event)
+titleset_event.id = 'seotitle_event'
+titleset_event.label = _(u'label_seotitle_event', default=u'Event')
+
+titleset_file = FormFieldsets(ISEOConfigTitleSchema_file)
+titleset_file.id = 'seotitle_file'
+titleset_file.label = _(u'label_seotitle_file', default=u'File')
+
+titleset_folder = FormFieldsets(ISEOConfigTitleSchema_folder)
+titleset_folder.id = 'seotitle_folder'
+titleset_folder.label = _(u'label_seotitle_folder', default=u'Folder')
+
+titleset_image = FormFieldsets(ISEOConfigTitleSchema_image)
+titleset_image.id = 'seotitle_image'
+titleset_image.label = _(u'label_seotitle_image', default=u'Image')
+
+titleset_link = FormFieldsets(ISEOConfigTitleSchema_link)
+titleset_link.id = 'seotitle_link'
+titleset_link.label = _(u'label_seotitle_link', default=u'Link')
+
+titleset_newsItem = FormFieldsets(ISEOConfigTitleSchema_newsItem)
+titleset_newsItem.id = 'seotitle_newsItem'
+titleset_newsItem.label = _(u'label_seotitle_newsItem', default=u'News Item')
+
+titleset_topic = FormFieldsets(ISEOConfigTitleSchema_topic)
+titleset_topic.id = 'seotitle_topic'
+titleset_topic.label = _(u'label_seotitle_topic', default=u'Topic')
+
+titleset = FormFieldsets(titleset_homepage, titleset_singlepage, titleset_searchpage,
+                         titleset_notfoundpage, titleset_authorpage, titleset_sitemappage,
+                         titleset_accessibilitypage, titleset_contactpage, titleset_event,
+                         titleset_file, titleset_folder, titleset_image, titleset_link,
+                         titleset_newsItem, titleset_topic)
 titleset.id = 'seotitle'
 titleset.label = _(u'label_seotitle', default=u'Title')
 
@@ -598,7 +722,9 @@ class Text2ListWidget(TextAreaWidget):
             return u'\r\n'.join(list(value))
 
 class PerSEOConfig(ControlPanelForm):
-
+    
+    template = ViewPageTemplateFile('templates/seo-control-panel.pt')
+    
     form_fields = FormFieldsets(wmtoolsset, titleset, indexingset, sitemapxmlset, rssset)
     
     form_fields['googleWebmasterTools'].custom_widget = TextWidget
