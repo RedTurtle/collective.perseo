19ee882f5c3a9ee2db9db8f7dd06c363687cc04f
Andrew Mleczko <andrew@mleczko.net>
2012-07-13 14:00:27 +0200

    add sitemap image support

--- collective/perseo/browser/sitemap.py
+++ collective/perseo/browser/sitemap.py
@@ -1,54 +1,77 @@
 from plone.app.layout.sitemap.sitemap import SiteMapView
-from Products.CMFCore.utils import getToolByName
-
 from zope.component import queryAdapter
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from collective.perseo.browser.seo_config import ISEOConfigSchema
+
 
 class PerSEOSiteMapView (SiteMapView):
     """Creates the sitemap as explained in the specifications.
 
     http://www.sitemaps.org/protocol.php
     """
+    template = ViewPageTemplateFile('sitemap.xml')
+
     def __init__(self, context, request):
         super(PerSEOSiteMapView, self).__init__(context, request)
         self.gseo = queryAdapter(self.context, ISEOConfigSchema)
-        
+
     def get_gseo_field( self, field, default=None):
         """ Returned field from Plone SEO Configuration Control Panel Tool
         """
         if self.gseo:
             return getattr(self.gseo, field, default)
         return default
-        
+
     def perseo_included_types(self):
         return self.get_gseo_field('not_included_types',default=())
-        
+
+    def add_image(self, url, caption=None, title=None):
+        image = {'loc': url}
+        if caption:
+            image.update({'caption': caption})
+        if title:
+            image.update({'title': title})
+        return image
+
     def objects(self):
         """Returns the data to create the sitemap."""
-        
+
         catalog = getToolByName(self.context, 'portal_catalog')
-        
+        included_types = self.perseo_included_types()
+        image_types = ('Image',)
+
         for item in catalog.searchResults({'Language': 'all'}):
-            
-            included_types = self.perseo_included_types()
+
             if item.getIncludedInSitemapxml != None:
                 include = item.getIncludedInSitemapxml
             else:
                 include = item.portal_type in included_types
-            
+
             if include:
                 if item.getPrioritySitemapxml:
-                    yield {
+                    row = {
                         'loc': item.getURL(),
                         'lastmod': item.modified.ISO8601(),
                         #'changefreq': 'always', # hourly/daily/weekly/monthly/yearly/never
                         'priority': item.getPrioritySitemapxml,
                     }
                 else:
-                    yield {
+                    row = {
                         'loc': item.getURL(),
                         'lastmod': item.modified.ISO8601(),
                         #'changefreq': 'always', # hourly/daily/weekly/monthly/yearly/never
                         #'priority': 0.5, # 0.0 to 1.0
                     }
+                row['images'] = []
+                if hasattr(item, 'hasContentLeadImage') and item.hasContentLeadImage:
+                    url = '%s/leadImage' % item.getURL()
+                    row['images'].append(self.add_image(url, item.Description, item.Title))
+
+                if item.portal_type in image_types:
+                    url = '%s' % item.getURL()
+                    row['images'].append(self.add_image(url, item.Description, item.Title))
+
+                yield row
+
